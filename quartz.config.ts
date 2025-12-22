2f82f66ab147d61165766288b7315d5c11b52044
nonameshere3803 <github@a.smsn.me>
2025-12-22 08:25:49 -0500

    Quartz sync: Dec 22, 2025, 8:25 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://nonameshere3803.github.io/players_guide/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
