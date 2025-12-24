f51e81feb1dab693b5a0eef50a569d874e9dec40
nonameshere3803 <github@a.smsn.me>
2025-12-24 08:19:11 -0500

    Quartz sync: Dec 24, 2025, 8:19 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -11,7 +11,7 @@ const config: QuartzConfig = {
     pageTitle: "Players Guide",
     pageTitleSuffix: "",
     enableSPA: true,
-    enablePopovers: true,
+    enablePopovers: false,
     analytics: {
       provider: "plausible",
     },
